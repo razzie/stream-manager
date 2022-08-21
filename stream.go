f5b6e99405bd07506c540630b05efbb6bdd67e4e
Gábor Görzsöny <gabor@gorzsony.com>
2022-08-21 03:15:32 +0200

    Bugfixes and improvements

--- stream.go
+++ stream.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os/exec"
+	"strings"
 	"sync/atomic"
 )
 
@@ -14,7 +15,7 @@ type Stream struct {
 	StartPosition string
 	Audio         int
 	Subtitle      int
-	runner        atomic.Value
+	runner        atomic.Pointer[StreamRunner]
 }
 
 func NewStream(name, source, target, startpos string, audio, subs int) (*Stream, error) {
@@ -33,57 +34,91 @@ func NewStream(name, source, target, startpos string, audio, subs int) (*Stream,
 func (stream *Stream) Start() error {
 	runner := NewStreamRunner(stream)
 	if !stream.runner.CompareAndSwap(nil, runner) {
-		return fmt.Errorf("stream already started")
+		for {
+			old := stream.runner.Load()
+			if old.IsRunning() {
+				return fmt.Errorf("stream already started")
+			}
+			if stream.runner.CompareAndSwap(old, runner) {
+				break
+			}
+		}
 	}
 	return runner.Start()
 }
 
 func (stream *Stream) Status() string {
-	runner := stream.runner.Load()
-	if runner != nil {
-		runner := runner.(*StreamRunner)
-		if err := runner.ctx.Err(); err != nil {
+	if runner := stream.runner.Load(); runner != nil {
+		if runner.IsRunning() {
+			return "Running"
+		} else if err := runner.Err(); err != nil {
 			return "Error: " + err.Error()
 		}
-		return "Running"
 	}
-	return "Pending"
+	return "Stopped"
 }
 
 func (stream *Stream) Close() error {
-	runner := stream.runner.Swap(nil)
-	if runner != nil {
-		runner := runner.(*StreamRunner)
-		return runner.Close()
+	if runner := stream.runner.Swap(nil); runner != nil {
+		runner.Close() // ignore exit status 1 error
 	}
 	return nil
 }
 
 type StreamRunner struct {
-	Stream *Stream
-	cmd    *exec.Cmd
-	ctx    context.Context
-	cancel context.CancelFunc
+	Stream  *Stream
+	cmd     *exec.Cmd
+	ctx     context.Context
+	cancel  context.CancelFunc
+	errChan chan error
+	errBuf  strings.Builder
+	done    atomic.Bool
 }
 
 func NewStreamRunner(stream *Stream) *StreamRunner {
 	ctx, cancel := context.WithCancel(context.Background())
 	cmd := exec.CommandContext(ctx, "ffmpeg", ffmpegArgs(stream)...)
 	return &StreamRunner{
-		Stream: stream,
-		cmd:    cmd,
-		ctx:    ctx,
-		cancel: cancel,
+		Stream:  stream,
+		cmd:     cmd,
+		ctx:     ctx,
+		cancel:  cancel,
+		errChan: make(chan error, 1),
 	}
 }
 
 func (runner *StreamRunner) Start() error {
-	return runner.cmd.Start()
+	runner.cmd.Stderr = &runner.errBuf
+	if err := runner.cmd.Start(); err != nil {
+		return err
+	}
+	go func() {
+		runner.errChan <- runner.cmd.Wait()
+		runner.done.Store(true)
+	}()
+	return nil
+}
+
+func (runner *StreamRunner) IsRunning() bool {
+	return !runner.done.Load()
+}
+
+func (runner *StreamRunner) Err() error {
+	if runner.done.Load() {
+		errStr := runner.errBuf.String()
+		if idx := strings.LastIndex(errStr, "\n"); idx != -1 {
+			errStr = errStr[idx+1:]
+		}
+		if len(errStr) > 0 {
+			return fmt.Errorf("%s", errStr)
+		}
+	}
+	return nil
 }
 
 func (runner *StreamRunner) Close() error {
 	runner.cancel()
-	return runner.cmd.Wait()
+	return <-runner.errChan
 }
 
 func ffmpegArgs(stream *Stream) (args []string) {
@@ -91,17 +126,18 @@ func ffmpegArgs(stream *Stream) (args []string) {
 		args = append(args, "-ss", stream.StartPosition)
 	}
 	args = append(args,
-		"-hide_banner", "-copyts", "-re",
+		"-hide_banner", "-loglevel", "error",
+		"-copyts", "-re",
 		"-i", stream.Source,
 		"-c:v", "libx264", "-preset", "ultrafast")
 	if len(stream.StartPosition) > 0 {
 		args = append(args, "-ss", stream.StartPosition)
 	}
 	if stream.Subtitle >= 0 {
-		args = append(args, fmt.Sprintf("subtitles=%s:stream_index=%d", stream.Source, stream.Subtitle))
+		args = append(args, "-vf", fmt.Sprintf("subtitles=%s:stream_index=%d", stream.Source, stream.Subtitle))
 	}
 	args = append(args,
-		"-c:a", "aac", "-map", fmt.Sprintf("0:a:%d", stream.Audio),
+		"-c:a", "aac", "-map", fmt.Sprintf("0:a:%d", stream.Audio), "-map", "0:v:0",
 		"-f", "rtsp", "-rtsp_transport", "tcp", "-auth_type", "digest",
 		stream.Target+"/"+stream.Name)
 	return
