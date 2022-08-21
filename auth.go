6b6efe9fbdd55f22b3512b37e2ceb5c0fc57dffa
Gábor Görzsöny <gabor@gorzsony.com>
2022-08-21 03:23:56 +0200

    Implemented auth middleware

--- auth.go
+++ auth.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/razzie/beepboop"
 )
 
+func DummyMiddleware(r *beepboop.PageRequest) *beepboop.View {
+	return nil
+}
+
 func AuthMiddleware(username, password string) beepboop.Middleware {
+	if len(username) == 0 && len(password) == 0 {
+		return DummyMiddleware
+	}
 	return func(r *beepboop.PageRequest) *beepboop.View {
+		reqUser, reqPass, ok := r.Request.BasicAuth()
+		if !ok || reqUser != username || reqPass != password {
+			return r.ErrorView("Unauthorized", http.StatusUnauthorized, beepboop.WithHeader("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`))
+		}
 		return nil
 	}
 }
